14d2b1c38867c1bca648bf4285f14676f2c9766c
eduard.garcia <edugarcas@gmail.com>
2017-05-03 17:12:47 +0200

    Added the option to look for another station in case the closest one doesn't have free bikes

--- bot_bicing_gram.rb
+++ bot_bicing_gram.rb
@@ -28,12 +28,17 @@ class BotMessage
       bot.api.send_message(chat_id: chatId, text: %Q{Ups! there aren't free bikes there try another station see if anyone available there})
     end
   end
-
 end
 
 Telegram::Bot::Client.run(TELEGRAM_BOT_TOKEN) do |bot|
   station     = Array.new
   remote_call = BicingStations.new
+
+  def bot_markup
+    kb = [Telegram::Bot::Types::KeyboardButton.new(text: 'Share my location', request_location: true),
+          [Telegram::Bot::Types::KeyboardButton.new(text: 'Closest station'),Telegram::Bot::Types::KeyboardButton.new(text: 'Next station')]]
+    Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+  end
 
   bot.listen do |message|
     if message.location
@@ -43,19 +48,16 @@ Telegram::Bot::Client.run(TELEGRAM_BOT_TOKEN) do |bot|
     end
     case message.text
       when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Thanks for using BotBicingram. Share your location and I'll show you the closest station" , reply_markup: markup)
+        bot.api.send_message(chat_id: message.chat.id, text: "Thanks for using BotBicingram. Share your location and I'll show you the closest station" , reply_markup: bot_markup)
       when '/station'
-        kb = [Telegram::Bot::Types::KeyboardButton.new(text: 'Share my location', request_location: true),
-              [Telegram::Bot::Types::KeyboardButton.new(text: 'Closest station'),Telegram::Bot::Types::KeyboardButton.new(text: 'Next station')]]
-        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
-        bot.api.send_message(chat_id: message.chat.id, text: 'Hey! Could you please share your location?', reply_markup: markup)
+        bot.api.send_message(chat_id: message.chat.id, text: 'Hey! Could you please share your location?', reply_markup: bot_markup)
       when 'Closest station'
         BotMessage.send_station_information(bot, message.chat.id, station)
       when 'Next station'
         BotMessage.send_station_information(bot, message.chat.id, station, 1)
       else
         unless (message.text.nil? || message.text.start_with?('At'))
-          bot.api.send_message(chat_id: message.chat.id, text: "Sorry, don't know what #{message.text} means, try typing /station instead.", reply_markup: markup)
+          bot.api.send_message(chat_id: message.chat.id, text: "Sorry, don't know what #{message.text} means, try typing /station instead.", reply_markup: bot_markup)
         end
     end
   end
